97f72315fdc475319d269899a4ba8b86fdf52d9b
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-01-19 18:36:38 +0530

    Disjoint set by size implementation.

--- Graph/disjointSetDataStructure.cpp
+++ Graph/disjointSetDataStructure.cpp
@@ -45,15 +45,16 @@ void _print(lld t) { cerr << t; }
 void _print(double t) { cerr << t; }
 void _print(ull t) { cerr << t; }
 
-// Disjoint set by rank data structure
+// Disjoint set data structure
 
 class DisjointSet {
-  vector<int> rank, parent;
+  vector<int> rank, size, parent;
 
 public:
   DisjointSet(int n) {
 
     rank.resize(n + 1, 0);
+    size.resize(n + 1, 1);
     parent.resize(n + 1);
     for (int i = 0; i <= n; i++) {
       parent[i] = i;
@@ -85,6 +86,23 @@ public:
       rank[ulp_u]++;
     }
   }
+
+  void unionBySize(int u, int v) {
+    int ulp_u = findParent(u);
+    int ulp_v = findParent(v);
+
+    if (ulp_u == ulp_v) {
+      return;
+    }
+
+    if (size[ulp_u] < size[ulp_v]) {
+      parent[ulp_u] = ulp_v;
+      size[ulp_v] += size[ulp_u];
+    } else {
+      parent[ulp_v] = ulp_u;
+      size[ulp_u] += size[ulp_v];
+    }
+  }
 };
 
 int main() {
@@ -93,18 +111,18 @@ int main() {
 #endif
 
   DisjointSet ds(7);
-  ds.unionByRank(1, 2);
-  ds.unionByRank(2, 3);
-  ds.unionByRank(4, 5);
-  ds.unionByRank(6, 7);
-  ds.unionByRank(5, 6);
+  ds.unionBySize(1, 2);
+  ds.unionBySize(2, 3);
+  ds.unionBySize(4, 5);
+  ds.unionBySize(6, 7);
+  ds.unionBySize(5, 6);
   // if 3 and 7 same compo or not
   if (ds.findParent(3) == ds.findParent(7)) {
     cout << "Same" << endl;
   } else {
     cout << "Not same" << endl;
   }
-  ds.unionByRank(3, 7);
+  ds.unionBySize(3, 7);
   // if 3 and 7 same compo or not
   if (ds.findParent(3) == ds.findParent(7)) {
     cout << "Same" << endl;

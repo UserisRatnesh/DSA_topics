96010c9d1791017b5176f91df29ae0cfbd3f6105
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-02-25 15:27:50 +0530

    Longest subarray with sum k. Count number of subarrays with sum k.

---
+++ array/longest_subarray_with_sum_k.cpp
@@ -0,0 +1,157 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nline "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+// NOTE: Longest subarray whose sum is equals to k
+// Elements can be +, - or zero
+int longestSubarray(vector<int> &nums, int k) {
+
+  int n = nums.size();
+  map<int, int> mpp;
+
+  int sum = 0;
+  int ans = 0;
+
+  for (int i = 0; i < n; ++i) {
+    sum += nums[i];
+    if (sum == k) {
+      ans = max(ans, i + 1);
+    } else if (mpp.find(sum - k) != mpp.end()) {
+      ans = max(ans, i - mpp[sum - k]);
+    }
+
+    if (mpp.find(sum) == mpp.end()) { // to maintain first occurance of this sum
+      mpp[sum] = i;
+    }
+  }
+
+  return ans;
+}
+
+// NOTE: Total number of subarray whose sum is equals to k
+// Elements can be +, - or zero
+int subarraySum(vector<int> &nums, int k) {
+  int n = nums.size();
+  map<int, int> mpp;
+
+  int sum = 0;
+  int ans = 0;
+  mpp[0] = 1;
+
+  for (int i = 0; i < n; ++i) {
+    sum += nums[i];
+    if (mpp.find(sum - k) != mpp.end()) {
+      ans += mpp[sum - k];
+    }
+
+    mpp[sum]++;
+  }
+
+  return ans;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  // freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  fastio();
+}

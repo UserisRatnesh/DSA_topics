7f3e6d7389ce3d09d89b8ff17b81189fd4acf3c9
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-07-02 19:07:17 +0530

    Sudoku solver.

---
+++ Recursion/sudoku_solver.cpp
@@ -0,0 +1,161 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+bool isSafe(int row, int col, char digit, vector<vector<char>> &board) {
+
+  for (int i = 0; i < 9; i++) {
+    if (board[row][i] == digit || board[i][col] == digit) {
+      // Digit already in row or column
+      return false;
+    }
+  }
+  int startRow = (row / 3) * 3;
+  int startCol = (col / 3) * 3;
+  for (int i = startRow; i < startRow + 3; i++) {
+    for (int j = startCol; j < startCol + 3; j++) {
+      if (board[i][j] == digit) {
+        // Digit already in 3x3 sub-box
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+bool helper(vector<vector<char>> &board) {
+
+  for (int i = 0; i < 9; i++) {
+    for (int j = 0; j < 9; j++) {
+      if (board[i][j] != '.') {
+        continue;
+      }
+
+      for (char c = '1'; c <= '9'; c++) {
+        if (isSafe(i, j, c, board)) {
+
+          board[i][j] = c;
+          bool ans = helper(board);
+
+          if (ans) {
+            return true;
+          }
+          board[i][j] = '.';
+        }
+      }
+
+      // Agar is {i,j}='.' ke liye koi digit possible nahi hai to return false
+      return false;
+    }
+  }
+
+  return true;
+}
+
+void solve_sudoku(vector<vector<char>> &board) { helper(board); }
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  fastio();
+}

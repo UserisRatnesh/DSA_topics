cc0d9bef43d7b1698143dccf5a9c372cf266dc32
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-07-02 12:46:22 +0530

    M coloring.

---
+++ Recursion/m_coloring.cpp
@@ -0,0 +1,159 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define fastio()                                                               \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+bool isSafe(int node, int color, vector<int> &colors,
+            vector<vector<int>> &adj) {
+  for (auto child : adj[node]) {
+    if (colors[child] == color) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+bool helper(int node, int m, vector<int> &colors, vector<vector<int>> &adj) {
+
+  int n = colors.size();
+  if (node == n) {
+    return true;
+  }
+
+  for (int c = 0; c < m; c++) {
+    if (isSafe(node, c, colors, adj)) {
+      colors[node] = c;
+      bool ans = helper(node + 1, m, colors, adj);
+      colors[node] = -1;
+
+      if (ans) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
+bool graphColoring(vector<vector<int>> &edges, int m, int n) {
+
+  vector<vector<int>> adj(n, vector<int>());
+  for (auto it : edges) {
+    int u = it[0];
+    int v = it[1];
+    adj[u].push_back(v);
+    adj[v].push_back(u);
+  }
+
+  vector<int> colors(n, -1);
+
+  bool ans = helper(0, m, colors, adj);
+
+  return ans;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  fastio();
+}

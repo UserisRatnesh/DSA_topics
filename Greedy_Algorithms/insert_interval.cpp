cc6437c5d689dd6586503ec7d7a24b7b61f81f27
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-07-29 13:37:28 +0530

    Insert interval.

---
+++ Greedy_Algorithms/insert_interval.cpp
@@ -0,0 +1,154 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define ratnesh()                                                              \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+vector<vector<int>> insertNewInterval(vector<vector<int>> &intervals,
+                                      vector<int> &newInterval) {
+
+  vector<vector<int>> vec(intervals.begin(), intervals.end());
+  vec.push_back(newInterval);
+
+  // sort on the basis of start time
+  sort(vec.begin(), vec.end(),
+       [](const vector<int> &v1, const vector<int> &v2) {
+         return v1[0] < v2[0];
+       });
+
+  int n = vec.size();
+
+  vector<vector<int>> ans;
+
+  int startTime = vec[0][0];
+  int endTime = vec[0][1];
+
+  for (int i = 1; i < n; i++) {
+
+    int newStartTime = vec[i][0];
+    int newEndTime = vec[i][1];
+
+    if (newStartTime <= endTime) {
+      // continue this interval
+      endTime = max(endTime, newEndTime);
+    } else {
+      ans.push_back({startTime, endTime});
+      startTime = newStartTime;
+      endTime = newEndTime;
+    }
+  }
+
+  ans.push_back({startTime, endTime});
+  return ans;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  // freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  ratnesh();
+
+  vector<vector<int>> intervals = {{1, 3}, {6, 9}};
+  vector<int> newInterval = {2, 5};
+  vector<vector<int>> ans = insertNewInterval(intervals, newInterval);
+  debug(ans);
+}

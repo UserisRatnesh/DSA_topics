7b83efb8b6b72346d8e795af82fffcb46bdb6004
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-08-08 23:15:15 +0530

    Count number of nice subarray.

---
+++ Sliding_window_and_2_pointer/nice_subarray.cpp
@@ -0,0 +1,171 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define ratnesh()                                                              \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+// BETTER: TC = O(n), SC = O(n)
+int numberOfOddSubarrays(vector<int> &nums, int k) {
+
+  int n = nums.size();
+  map<int, int> oddCount; //{odd, freq}
+  oddCount[0] = 1;
+  int cnt = 0;
+  int ans = 0;
+  for (auto it : nums) {
+    if (it % 2 == 1) {
+      cnt++;
+    }
+    if (oddCount.count(cnt - k)) {
+      ans += oddCount[cnt - k];
+    }
+    oddCount[cnt]++;
+  }
+
+  return ans;
+}
+
+// OPTIMAL: TC = O(n), SC = (1)
+
+int helper(vector<int> &nums, int k) {
+
+  if (k < 0) {
+    return 0;
+  }
+
+  int n = nums.size();
+  int r = 0;
+  int l = 0;
+  int ans = 0;
+  int oddCnt = 0;
+
+  while (r < n) {
+
+    if (nums[r] % 2 == 1) {
+      oddCnt++;
+    }
+
+    while (oddCnt > k) {
+      if (nums[l] % 2 == 1) {
+        oddCnt--;
+      }
+      l++;
+    }
+
+    ans += (r - l + 1);
+    r++;
+  }
+
+  return ans;
+}
+
+int numberOfOddSubarrays_op(vector<int> &nums, int k) {
+
+  return helper(nums, k) - helper(nums, k - 1);
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  ratnesh();
+}

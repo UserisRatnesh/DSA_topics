f9d16e20b6ddc328c1c4738c8252bb31d0faf6f2
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-08-12 19:37:35 +0530

    First commit of Dynamic programming.

---
+++ Dynamic_Programming/climb_stairs.cpp
@@ -0,0 +1,164 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define ratnesh()                                                              \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+// NOTE: Recursion
+int climbStairs(int n) {
+
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+
+  return climbStairs(n - 1) + climbStairs(n - 2);
+}
+
+// NOTE: Memoization
+// TC: O(n), SC: O(n) + O(n)
+int climbStairs_memo(int n, vector<int> &dp) {
+
+  if (n == 0 || n == 1) {
+    return 1;
+  }
+
+  if (dp[n] != -1) {
+    return dp[n];
+  }
+
+  return dp[n] = climbStairs_memo(n - 1, dp) + climbStairs_memo(n - 2, dp);
+}
+
+// NOTE: Tabulation
+// TC: O(n), SC: O(n)
+int climbStairs_tab(int n) {
+
+  vector<int> dp(n + 1, -1);
+  dp[0] = 1;
+  dp[1] = 1;
+  for (int i = 2; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+
+  return dp[n];
+}
+
+// NOTE: Space optimization
+// TC: O(n), SC: O(1)
+int climbStairs_op(int n) {
+
+  int prev2 = 1;
+  int prev = 1;
+  for (int i = 2; i <= n; i++) {
+    int curr = prev2 + prev;
+    prev2 = prev;
+    prev = curr;
+  }
+  return prev;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  ratnesh();
+}

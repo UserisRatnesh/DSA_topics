f88b7f51134f755f02f90f128ce401b7c69ef3e7
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2025-08-20 13:03:12 +0530

    House robber.

---
+++ Dynamic_Programming/house_robber.cpp
@@ -0,0 +1,152 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define ratnesh()                                                              \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+// NOTE: max sum of non adjacent elements in array
+int max_non_adjacent_sum(vector<int> &nums) {
+
+  int n = nums.size();
+  int prev_pick = 0;
+  int prev_not_pick = 0;
+
+  for (int i = 0; i < n; i++) {
+
+    int pick = prev_not_pick + nums[i];
+    int not_pick = max(prev_pick, prev_not_pick);
+
+    prev_pick = pick;
+    prev_not_pick = not_pick;
+  }
+
+  return max(prev_not_pick, prev_pick);
+}
+
+// HACK: Whether it is Recursion, memoization, tabulation or space optimized
+// code it depends on the code of max_non_adjacent_sum() fun
+int house_robber(vector<int> &money) {
+
+  int n = money.size();
+  if (n == 1) {
+    return money[0];
+  }
+
+  vector<int> first, second;
+  for (int i = 0; i < n; i++) {
+    if (i != 0) {
+      first.push_back(money[i]);
+    }
+    if (i != n - 1) {
+      second.push_back(money[i]);
+    }
+  }
+
+  return max(max_non_adjacent_sum(first), max_non_adjacent_sum(second));
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  ratnesh();
+}

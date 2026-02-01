c589f6536673db6f2f51f56db82d0db204d1449d
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2026-02-01 20:06:14 +0530

    Longest String Chain.

---
+++ Dynamic_Programming/longest_string_chain.cpp
@@ -0,0 +1,161 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define ratnesh()                                                              \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+bool isPossible(string &s1, string &s2) {
+
+  int n1 = s1.size();
+  int n2 = s2.size();
+
+  if (n1 != n2 + 1) {
+    return false;
+  }
+
+  int i = 0, j = 0;
+  int diff = 0;
+
+  // HACK: Ye issliye kaam kar rha kyoki s2 ka size s1 se sirf ek hi jyada hoga,
+  // Ye humne pahle hi chekc kar liya hai.
+  while (i < n1) {
+    if (s1[i] == s2[j]) {
+      i++;
+      j++;
+    } else {
+      i++;
+    }
+  }
+
+  return i == n1 && j == n2;
+}
+
+bool static compare(string &s1, string &s2) { return s1.size() < s2.size(); }
+
+int longestStringChain(vector<string> &words) {
+
+  sort(words.begin(), words.end(), compare);
+  int n = words.size();
+  vector<int> dp(n + 1, 1);
+  int maxi = 1;
+
+  for (int i = 0; i < n; i++) {
+    for (int j = 0; j < i; j++) {
+
+      // Saath me jo jo characters words[j] me hai whi characters words[j] me
+      // hone chahiye
+      if (isPossible(words[i], words[j]) && dp[i] < dp[j] + 1) {
+        dp[i] = dp[j] + 1;
+      }
+    }
+    maxi = max(maxi, dp[i]);
+  }
+
+  return maxi;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  ratnesh();
+}

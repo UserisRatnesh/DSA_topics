60c498a53444c8a1bf805560bc136d00e4bfc8eb
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2026-02-03 23:18:15 +0530

    Longest common substring using Longest common subsequence code.

---
+++ Dynamic_Programming/longest_common_substring.cpp
@@ -0,0 +1,162 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define ratnesh()                                                              \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+// NOTE: Recursion
+
+// NOTE: Memoization
+// HACK: 3-D dp lena padega isliye useless hai
+
+// NOTE: Tabulation
+// TC: O(n * m)   SC: O(n * m)
+int Tabulation(string str1, string str2) {
+  int n = str1.size();
+  int m = str2.size();
+
+  int ans = 0;
+  vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+  for (int i = n - 1; i >= 0; i--) {
+    for (int j = m - 1; j >= 0; j--) {
+      if (str1[i] == str2[j]) {
+        dp[i][j] = 1 + dp[i + 1][j + 1];
+        ans = max(ans, dp[i][j]);
+      } else {
+        dp[i][j] = 0; // Kyoki break aa jaa rha hai
+      }
+    }
+  }
+
+  return ans;
+}
+
+// NOTE: Space optmization
+// TC: O(n * m)   SC: O(m)
+int space(string str1, string str2) {
+  int n = str1.size();
+  int m = str2.size();
+  int ans = 0;
+  vector<int> next(m + 1, 0);
+
+  for (int i = n - 1; i >= 0; i--) {
+    vector<int> curr(m + 1, 0);
+    for (int j = m - 1; j >= 0; j--) {
+      if (str1[i] == str2[j]) {
+        curr[j] = 1 + next[j + 1];
+        ans = max(ans, curr[j]);
+      } else {
+        curr[j] = 0;
+      }
+    }
+    next = curr;
+  }
+
+  return ans;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  ratnesh();
+}

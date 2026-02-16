c4c68277d04e8147eb9eed4c4486fb8293951eb1
Ratnesh singh <ratneshsingh8521127312@gmail.com>
2026-02-16 15:55:09 +0530

    Karatsuba Algorithm for multiplication.

---
+++ ExtraAlgorithms/karatsuba_algo.cpp
@@ -0,0 +1,164 @@
+#ifndef ONLINE_JUDGE
+#include "/usr/local/include/bits/stdc++.h"
+#else
+#include <bits/stdc++.h>
+#endif
+
+using namespace std;
+
+#define ratnesh()                                                              \
+  ios_base::sync_with_stdio(false);                                            \
+  cin.tie(NULL);                                                               \
+  cout.tie(NULL)
+#define MOD 1000000007
+#define MOD1 998244353
+#define INF 1e18
+#define nl "\n"
+#define pb push_back
+#define ppb pop_back
+#define mp make_pair
+#define ff first
+#define ss second
+#define PI 3.141592653589793238462
+#define set_bits __builtin_popcountll
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
+#define yes cout << "YES\n"
+#define no cout << "NO\n"
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef long double lld;
+
+#ifndef ONLINE_JUDGE
+#define debug(x)                                                               \
+  cerr << #x << " ";                                                           \
+  _print(x);                                                                   \
+  cerr << endl;
+#else
+#define debug(x)
+#endif
+
+void _print(ll t) { cerr << t; }
+void _print(int t) { cerr << t; }
+void _print(string t) { cerr << t; }
+void _print(char t) { cerr << t; }
+void _print(lld t) { cerr << t; }
+void _print(double t) { cerr << t; }
+void _print(ull t) { cerr << t; }
+
+template <class T, class V> void _print(pair<T, V> p);
+template <class T> void _print(vector<T> v);
+template <class T> void _print(set<T> v);
+template <class T, class V> void _print(map<T, V> v);
+template <class T> void _print(multiset<T> v);
+template <class T, class V> void _print(pair<T, V> p) {
+  cerr << "{";
+  _print(p.ff);
+  cerr << ",";
+  _print(p.ss);
+  cerr << "}";
+}
+template <class T> void _print(vector<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(set<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T> void _print(multiset<T> v) {
+  cerr << "[ ";
+  for (T i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+template <class T, class V> void _print(map<T, V> v) {
+  cerr << "[ ";
+  for (auto i : v) {
+    _print(i);
+    cerr << " ";
+  }
+  cerr << "]";
+}
+
+/*
+   _____          _        ____       _
+  / ____|        | |      |  _ \     | |
+ | |     ___   __| | ___  | |_) | ___| | _____      __
+ | |    / _ \ / _` |/ _ \ |  _ < / _ \ |/ _ \ \ /\ / /
+ | |___| (_) | (_| |  __/ | |_) |  __/ | (_) \ V  V /
+  \_____\___/ \__,_|\___| |____/ \___|_|\___/ \_/\_/
+
+*/
+
+// TOPIC: Karatsuba Multiplication
+// TC: O(n^1.59) n=No of digits, b=base(10 for Decimal number)
+ll getLength(ll X) {
+  if (X == 0)
+    return 1;
+  ll len = 0;
+  while (X > 0) {
+    len++;
+    X /= 10;
+  }
+  return len;
+}
+
+// Integer power of 10 (avoids floating pow bug)
+ll powerBase(ll n) {
+  ll res = 1;
+  ll base = 10;
+  while (n--) {
+    res *= base;
+  }
+  return res;
+}
+
+ll karatsuba(ll X, ll Y) {
+  if (X < 10 && Y < 10) {
+    return X * Y;
+  }
+
+  ll maxLen = max(getLength(X), getLength(Y));
+  ll halfLen = (maxLen + 1) / 2;
+
+  ll multiplier = powerBase(halfLen);
+
+  ll a = X / multiplier;
+  ll b = X % multiplier;
+  ll c = Y / multiplier;
+  ll d = Y % multiplier;
+
+  ll s1 = karatsuba(a, c);
+  ll s2 = karatsuba(b, d);
+  ll s3 = karatsuba(a + b, c + d);
+
+  ll s4 = s3 - s1 - s2;
+
+  return s1 * powerBase(2 * halfLen) + s4 * multiplier + s2;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("Error.txt", "w", stderr);
+  freopen("input.txt", "r", stdin);
+#endif
+  ratnesh();
+
+  int X = 1026732;
+  int Y = 732912;
+
+  ll ans = karatsuba(X, Y);
+  cout << ans << nl;
+}
